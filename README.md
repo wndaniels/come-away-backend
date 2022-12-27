b53e87e6b9493b96573b37f719a9b2a84b7fc5f4
Nathan Daniels <85182159+wndaniels@users.noreply.github.com>
2022-12-26 23:02:19 -0500

    Initial push to GitHub.

---
+++ README.md
@@ -0,0 +1,4 @@
+# WELCOME TO COME/AWAY
+
+## Description: 
+Come/Away is a React application created with the sole purpose of streamlining visiting times for the family and friends of new parents. With Come/Away, those expecting a baby are able to simplify the way they share their good news when the baby arrives. The expecting parent(s) after setting up an account will set their expected due date, a special message regarding their babies arrival, as well as optimal visiting hours. The user will then be able to share a link with their loved ones containing a form to join a mailing list. Upon babies arrival, the parent will share their announcement email which will also contain a link directing recipients to a calendar to select a time they plan to visit. 
---
+++ come-away-schema.sql
@@ -0,0 +1,62 @@
+﻿CREATE TABLE users (
+    username VARCHAR(25) PRIMARY KEY,
+    password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    due_date_id INTEGER 
+        REFERENCES due_date ON DELETE CASCADE,
+    calendar_id INTEGER 
+        REFERENCES calendar ON DELETE CASCADE,
+    mailing_list_id INTEGER 
+        REFERENCES mailing_list ON DELETE CASCADE
+);
+
+CREATE TABLE due_date (
+    id INTEGER PRIMARY KEY,
+    month TEXT NOT NULL,
+    day TEXT NOT NULL,
+    year TEXT NOT NULL
+);
+
+CREATE TABLE calendar (
+    id INTEGER PRIMARY KEY,
+    day_id INTEGER 
+        REFERENCES day ON DELETE CASCADE,
+    month_id INTEGER 
+        REFERENCES month ON DELETE CASCADE,
+    year_id INTEGER 
+        REFERENCES year ON DELETE CASCADE
+);
+
+CREATE TABLE day (
+    id INTEGER PRIMARY KEY,
+    day TEXT NOT NULL
+);
+
+CREATE TABLE month (
+    id INTEGER PRIMARY KEY,
+    month TEXT NOT NULL
+);
+
+CREATE TABLE year (
+    id INTEGER PRIMARY KEY,
+    year TEXT NOT NULL
+);
+
+CREATE TABLE mailing_list (
+    id INTEGER PRIMARY KEY,
+    guest_id INTEGER 
+        REFERENCES guest ON DELETE CASCADE
+);
+
+CREATE TABLE guest (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    gift BOOLEAN DEFAULT FALSE
+    meal BOOLEAN DEFAULT FALSE
+);
+
+
