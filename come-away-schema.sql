7dbcb89c1255dc7dd3f76b89bc1ee63f66633fa8
Nathan Daniels <85182159+wndaniels@users.noreply.github.com>
2022-12-26 23:47:14 -0500

    Update to schema.

--- come-away-schema.sql
+++ come-away-schema.sql
@@ -3,7 +3,8 @@
     password TEXT NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    email TEXT NOT NULL,
+    email TEXT NOT NULL
+        CHECK (position('@' IN email) > 1),
     due_date_id INTEGER 
         REFERENCES due_date ON DELETE CASCADE,
     calendar_id INTEGER 
@@ -15,31 +16,38 @@
 CREATE TABLE due_date (
     id INTEGER PRIMARY KEY,
     month TEXT NOT NULL,
-    day TEXT NOT NULL,
+    days TEXT NOT NULL,
     year TEXT NOT NULL
 );
 
 CREATE TABLE calendar (
     id INTEGER PRIMARY KEY,
     day_id INTEGER 
-        REFERENCES day ON DELETE CASCADE,
+        REFERENCES days ON DELETE CASCADE,
+    day_of_week_id INTEGER
+        REFERENCES days_of_week ON DELETE CASCADE,
     month_id INTEGER 
-        REFERENCES month ON DELETE CASCADE,
+        REFERENCES months ON DELETE CASCADE,
     year_id INTEGER 
-        REFERENCES year ON DELETE CASCADE
+        REFERENCES years ON DELETE CASCADE
 );
 
-CREATE TABLE day (
+CREATE TABLE days (
     id INTEGER PRIMARY KEY,
     day TEXT NOT NULL
 );
 
-CREATE TABLE month (
+CREATE TABLE days_of_week (
+    id INTEGER PRIMARY KEY,
+    day_of_week TEXT NOT NULL
+)
+
+CREATE TABLE months (
     id INTEGER PRIMARY KEY,
     month TEXT NOT NULL
 );
 
-CREATE TABLE year (
+CREATE TABLE years (
     id INTEGER PRIMARY KEY,
     year TEXT NOT NULL
 );
