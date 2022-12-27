a188799131692cc89c82486d59d3764d90010764
Nathan Daniels <85182159+wndaniels@users.noreply.github.com>
2022-12-26 23:48:58 -0500

    Create DB file

---
+++ come-away.sql
@@ -0,0 +1,18 @@
+\echo 'Delete and recreate come_away db?'
+\prompt 'Return for yes or control-C for cancel >' foo
+
+DROP DATABASE come_away;
+CREATE DATABASE come_away;
+\connect come_away
+
+\i come-away-schema.sql
+\i come-away-seed.sql
+
+\echo 'Delete and recreate come_away_test db?'
+\prompt 'Return for yes or control-C for cancel >' foo
+
+DROP DATABASE come_away_test;
+CREATE DATABASE come_away_test;
+\connect come_away_test
+
+\i come-away-schema.sql
