6a6a8b3734d9cc089ccb323da280ba8018d6a573
Nathan Daniels <85182159+wndaniels@users.noreply.github.com>
2023-06-26 12:55:12 -0400

    updates to unit testing

--- routes/__test__/dueDate.test.js
+++ routes/__test__/dueDate.test.js
@@ -8,6 +8,7 @@ const { ensureCorrectUserOrAdmin } = require("../../middleware/auth");
 const Calendar = require("../../models/calendar");
 const DueDate = require("../../models/dueDate");
 const { BadRequestError } = require("../../expressError");
+const db = require("../../db");
 
 // Mock the Calendar model methods
 jest.mock("../../models/calendar", () => ({
@@ -28,7 +29,10 @@ let token;
 let mockDueDate;
 
 beforeEach(() => {
+  // Generate a JWT token for authentication
   token = jwt.sign({ username: "testuser", isAdmin: false }, SECRET_KEY);
+
+  // Prepare mock data for due date
   mockDueDate = [
     {
       id: 1,
@@ -48,23 +52,29 @@ describe("DueDate routes", () => {
 
   describe("GET /due-date/years", () => {
     it("should return years data", async () => {
+      // Mock the Calendar.getYearsData method to resolve with mock data
       const mockYearsData = ["2021", "2022", "2023"];
       Calendar.getYearsData.mockResolvedValueOnce(mockYearsData);
 
+      // Send a GET request to the specified route
       const response = await request(app).get("/due-date/years");
 
+      // Verify the response
       expect(response.statusCode).toBe(200);
       expect(response.body).toEqual(mockYearsData);
       expect(Calendar.getYearsData).toHaveBeenCalled();
     });
 
     it("should handle errors when retrieving years data", async () => {
+      // Mock the Calendar.getYearsData method to reject with an error
       Calendar.getYearsData.mockRejectedValueOnce(
         new Error("Error retrieving years data")
       );
 
+      // Send a GET request to the specified route
       const response = await request(app).get("/due-date/years");
 
+      // Verify the response
       expect(response.statusCode).toBe(500);
       expect(response.body).toEqual({
         error: {
@@ -75,25 +85,32 @@ describe("DueDate routes", () => {
       expect(Calendar.getYearsData).toHaveBeenCalled();
     });
   });
+
   describe("GET /due-date/months", () => {
     it("should return months data", async () => {
+      // Mock the Calendar.getMonthsData method to resolve with mock data
       const mockMonthsData = ["January", "February", "March"];
       Calendar.getMonthsData.mockResolvedValueOnce(mockMonthsData);
 
+      // Send a GET request to the specified route
       const response = await request(app).get("/due-date/months");
 
+      // Verify the response
       expect(response.statusCode).toBe(200);
       expect(response.body).toEqual(mockMonthsData);
       expect(Calendar.getMonthsData).toHaveBeenCalled();
     });
 
     it("should handle errors when retrieving months data", async () => {
+      // Mock the Calendar.getMonthsData method to reject with an error
       Calendar.getMonthsData.mockRejectedValueOnce(
         new Error("Error retrieving months data")
       );
 
+      // Send a GET request to the specified route
       const response = await request(app).get("/due-date/months");
 
+      // Verify the response
       expect(response.statusCode).toBe(500);
       expect(response.body).toEqual({
         error: {
@@ -107,23 +124,29 @@ describe("DueDate routes", () => {
 
   describe("GET /due-date/days", () => {
     it("should return days data", async () => {
+      // Mock the Calendar.getDaysData method to resolve with mock data
       const mockDaysData = [1, 2, 3, 4, 5];
       Calendar.getDaysData.mockResolvedValueOnce(mockDaysData);
 
+      // Send a GET request to the specified route
       const response = await request(app).get("/due-date/days");
 
+      // Verify the response
       expect(response.statusCode).toBe(200);
       expect(response.body).toEqual(mockDaysData);
       expect(Calendar.getDaysData).toHaveBeenCalled();
     });
 
     it("should handle errors when retrieving days data", async () => {
+      // Mock the Calendar.getDaysData method to reject with an error
       Calendar.getDaysData.mockRejectedValueOnce(
         new Error("Error retrieving days data")
       );
 
+      // Send a GET request to the specified route
       const response = await request(app).get("/due-date/days");
 
+      // Verify the response
       expect(response.statusCode).toBe(500);
       expect(response.body).toEqual({
         error: {
@@ -135,24 +158,30 @@ describe("DueDate routes", () => {
     });
   });
 
-  describe("GET /", () => {
+  describe("GET /due-date", () => {
     it("should return all due dates", async () => {
+      // Mock the DueDate.getAllDueDates method to resolve with mock data
       DueDate.getAllDueDates.mockResolvedValueOnce(mockDueDate);
 
+      // Send a GET request to the specified route
       const response = await request(app).get("/due-date");
 
+      // Verify the response
       expect(response.statusCode).toBe(200);
       expect(response.body).toEqual(mockDueDate);
       expect(DueDate.getAllDueDates).toHaveBeenCalled();
     });
 
     it("should handle errors when retrieving due dates", async () => {
+      // Mock the DueDate.getAllDueDates method to reject with an error
       DueDate.getAllDueDates.mockRejectedValueOnce(
         new Error("Error retrieving due dates")
       );
 
+      // Send a GET request to the specified route
       const response = await request(app).get("/due-date");
 
+      // Verify the response
       expect(response.statusCode).toBe(500);
       expect(response.body).toEqual({
         error: {
@@ -166,6 +195,7 @@ describe("DueDate routes", () => {
 
   describe("POST /due-date/:username/create", () => {
     it("should create a new due date", async () => {
+      // Prepare mock data for a new due date
       const mockNewDueDate = {
         babyName: "Test Baby 2",
         yearId: 1,
@@ -174,23 +204,28 @@ describe("DueDate routes", () => {
         userId: 1,
       };
 
+      // Mock the DueDate.create method to resolve with the mock new due date
       DueDate.create.mockResolvedValueOnce(mockNewDueDate);
 
+      // Send a POST request to the specified route with the necessary parameters and authentication token
       const response = await request(app)
         .post("/due-date/testuser/create")
         .set("Authorization", `Bearer ${token}`)
         .send(mockNewDueDate);
 
+      // Verify the response
       expect(response.statusCode).toBe(201);
       expect(response.body).toEqual({ dueDate: mockNewDueDate });
       expect(DueDate.create).toHaveBeenCalledWith(mockNewDueDate);
     });
 
     it("should handle errors when creating a due date", async () => {
+      // Mock the DueDate.create method to reject with an error
       DueDate.create.mockRejectedValueOnce(
         new Error("Error creating a due date")
       );
 
+      // Send a POST request to the specified route with the necessary parameters and authentication token
       const response = await request(app)
         .post("/due-date/testuser/create")
         .set("Authorization", `Bearer ${token}`)
@@ -202,6 +237,7 @@ describe("DueDate routes", () => {
           userId: 1,
         });
 
+      // Verify the response
       expect(response.statusCode).toBe(500);
       expect(response.body).toEqual({
         error: {
@@ -220,43 +256,54 @@ describe("DueDate routes", () => {
   });
 
   describe("PATCH /due-date/:id/:username/edit", () => {
-    it("should edit due date", async () => ({}));
-  });
-
-  describe("PATCH /due-date/:id/:username/edit", () => {
     it("should update a due date", async () => {
+      // Prepare mock data for an updated due date
       const mockUpdateDueDate = {
-        babyName: "Updated Name",
+        babyName: "New Name",
+        yearId: 1,
+        monthId: 2,
+        dayId: 3,
       };
 
+      // Mock the DueDate.update method to resolve with the mock updated due date
       DueDate.update.mockResolvedValueOnce(mockUpdateDueDate);
 
+      // Send a PATCH request to the specified route with the necessary parameters and authentication token
       const response = await request(app)
         .patch(`/due-date/${mockDueDate[0].id}/testuser/edit`)
         .set("Authorization", `Bearer ${token}`)
-        .send(1, mockUpdateDueDate);
+        .send(mockUpdateDueDate);
 
+      // Verify the response
       expect(response.statusCode).toBe(201);
       expect(response.body).toEqual({ updateDueDate: mockUpdateDueDate });
-      expect(DueDate.update).toHaveBeenCalledWith(1, mockUpdateDueDate);
+      expect(DueDate.update).toHaveBeenCalledWith(
+        `${mockDueDate[0].id}`,
+        mockUpdateDueDate
+      );
     });
 
     it("should handle errors when updating a due date", async () => {
+      // Prepare mock data for an updated due date
       const mockUpdateDueDate = {
-        babyName: "Updated Name",
+        babyName: "New Name",
+        yearId: 1,
+        monthId: 2,
+        dayId: 3,
       };
 
+      // Mock the DueDate.update method to reject with an error
       DueDate.update.mockRejectedValueOnce(
         new Error("Error updating a due date")
       );
 
+      // Send a PATCH request to the specified route with the necessary parameters and authentication token
       const response = await request(app)
-        .patch(`/due-date/${mockUpdateDueDate.id}/testuser/edit`)
+        .patch(`/due-date/${mockDueDate[0].id}/testuser/edit`)
         .set("Authorization", `Bearer ${token}`)
         .send(mockUpdateDueDate);
 
-      console.log(response.body);
-
+      // Verify the response
       expect(response.statusCode).toBe(500);
       expect(response.body).toEqual({
         error: {
@@ -264,25 +311,25 @@ describe("DueDate routes", () => {
           status: 500,
         },
       });
-      expect(DueDate.update).toHaveBeenCalledWith(mockDueDate.id, {
-        babyName: "UpdatedName",
-        yearId: 2,
-        monthId: 12,
-        dayId: 15,
-        userId: 1,
-      });
+      expect(DueDate.update).toHaveBeenCalledWith(
+        `${mockDueDate[0].id}`,
+        mockUpdateDueDate
+      );
     });
   });
 
   describe("DELETE /due-date/:id/:username/delete", () => {
-    it("should delete created due date", async () => {
+    it("should delete a due date", async () => {
+      // Mock the DueDate.delete method to resolve with the mock due date
       DueDate.delete.mockResolvedValueOnce(mockDueDate);
 
+      // Send a DELETE request to the specified route with the necessary parameters and authentication token
       const response = await request(app)
         .delete(`/due-date/${mockDueDate[0].id}/testuser/delete`)
         .set("Authorization", `Bearer ${token}`)
         .send(mockDueDate);
 
+      // Verify the response
       expect(response.statusCode).toBe(200);
       expect(response.body).toEqual({ delete: 1 });
     });
